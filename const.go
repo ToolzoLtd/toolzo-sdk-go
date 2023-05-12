c119d673dbe78c96f76217f702fad6df1a661e08
producttoolzo <product@toolzo.com>
2023-05-12 15:58:30 +0300

    HOTFIX: setup own credentials

--- const.go
+++ const.go
@@ -1,9 +1,9 @@
 package main
 
 const (
-	ApiUrl             = "https://apidomen/api/v1/"
+	ApiUrl             = "https://pay.toolzo.com"
 	ApiVersion         = "1"
-	ApiKey             = "47620c0e88f2f9f891293ad45"                                                       // your API KEY
-	Secret             = "D85SKVd7wWdDHfJgrLDry+9/yka1dc9fdXV2YN+J8ugt0ZJ4pZqUq9Dkcdvd/Ti0WgFgGVV3UTdUw==" // your SECRET (hmacKey)
+	ApiKey             = "your apikey"
+	Secret             = "your secret key"
 	RequestTypeRequest = "Request"
 )
